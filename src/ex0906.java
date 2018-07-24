0da5ded6f954606afb174f8a95b5e822ac5ce3c9
lehade <alexey.denisenko@me.com>
2018-07-24 21:37:37 +0300

    stktrace in mainmeth

---
+++ src/ex0906.java
@@ -0,0 +1,16 @@
+public class ex0906 {
+
+    public static void main(String[] args) {
+        log("In main method");
+    }
+
+    public static void log(String s) {
+        //напишите тут ваш код
+
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        System.out.println(stackTraceElements[2].getClassName() +": " + stackTraceElements[2].getMethodName()+": " + s);
+
+    }
+
+}
