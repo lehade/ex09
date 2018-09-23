66d8f07333c9bb8a083c9083e7d93bf1551265a4
lehade <alexey.denisenko@me.com>
2018-09-24 00:15:45 +0300

    exception with bean.log

---
+++ src/ex0915.java
@@ -0,0 +1,47 @@
+import java.io.CharConversionException;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+
+public class ex0915 {
+
+    public static StatelessBean BEAN = new StatelessBean();
+
+    public static void main(String[] args) {
+        try {
+            processExceptions();
+        } catch (Exception e) {
+            BEAN.log(e);
+        }
+    }
+
+    public static void processExceptions() throws FileSystemException {
+        try {
+            BEAN.methodThrowExceptions();
+        } catch (FileSystemException e) {
+            BEAN.log(e);
+            throw e;
+        } catch (CharConversionException e) {
+            BEAN.log(e);
+        } catch (IOException e) {
+            BEAN.log(e);
+        }
+    }
+
+
+    public static class StatelessBean {
+        public void log(Exception exception) {
+            System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
+        }
+
+        public void methodThrowExceptions() throws CharConversionException, FileSystemException, IOException {
+            int i = (int) (Math.random() * 3);
+            if (i == 0)
+                throw new CharConversionException();
+            if (i == 1)
+                throw new FileSystemException("");
+            if (i == 2)
+                throw new IOException();
+        }
+    }
+
+}
