aaa03615408873124b8b0b66c20b963da7b4e948
lehade <alexey.denisenko@me.com>
2018-09-23 22:56:10 +0300

    one exception for all

---
+++ src/ex0914.java
@@ -0,0 +1,34 @@
+public class ex0914 {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+        try {
+            method1();
+        }
+               catch (Exception e)
+        {
+            System.out.println("Any other errors. Exception has been caught.");
+        }
+
+    }
+
+    public static void method1() throws Exception1, Exception2, Exception3 {
+        int i = (int) (Math.random() * 3);
+        if (i == 0)
+            throw new Exception1();
+        if (i == 1)
+            throw new Exception2();
+        if (i == 2)
+            throw new Exception3();
+    }
+}
+
+class Exception1 extends Exception {
+}
+
+class Exception2 extends Exception1 {
+}
+
+class Exception3 extends Exception2 {
+}
