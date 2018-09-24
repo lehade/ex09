739411dd337209c2e83047f87abe0bbe847f1d38
lehade <alexey.denisenko@me.com>
2018-09-24 23:33:53 +0300

    countdown 100ms with try catch

---
+++ src/ex0920.java
@@ -0,0 +1,18 @@
+public class ex0920 {
+
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            }
+            catch (Exception e) {
+
+            }
+
+            //напишите тут ваш код
+        }
+    }
+
+}
