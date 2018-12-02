d3ca9000431482db4f50d5904e709f52c15b2702
lehade <alexey.denisenko@me.com>
2018-12-02 19:18:21 +0300

    var static ints

---
+++ src/ex0925.java
@@ -0,0 +1,19 @@
+public class ex0925 {
+
+    public static int A = 5;
+    public static int B = 2 * A;
+    public int C = A * B;
+    public static int D = A * B;
+
+    public static void main(String[] args) {
+        Solution room = new Solution();
+        room.A = 5;
+
+        Solution.D = 5;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+}
